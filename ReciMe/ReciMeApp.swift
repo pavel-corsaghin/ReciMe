e3e4b8c20a52f94880d425fc474e381e879ee62b
Hung Nguyen <hung.nguyen@vogappdevelopers.com>
2023-01-14 01:37:26 +0700

    Initial Commit

---
+++ ReciMe/ReciMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciMeApp.swift
+//  ReciMe
+//
+//  Created by HungNguyen on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct ReciMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
